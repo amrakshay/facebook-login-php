460a9cd010b421a6979868b4599b62eadb101ab4
amrakshay <amrakshay@gmail.com>
2018-11-14 14:23:15 +0530

    Update index.php
    Added Logout Button

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	</head>
     <body>
 	    <button id="loginBtn">Facebook Login</button>
+	    <button id="loginBtn">Facebook Logout</button>
 	    <button id="loginOut" style="display: none;" onclick="fbLogout()">Logout</button>
 	    <div id="response"></div>
 	    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" type="text/javascript"></script>
